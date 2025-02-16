3736e70920e3e82e0fc38d816137e43037c506a3
OrangeBacon <computer.backup.15@gmail.com>
2025-02-16 16:45:25 +0000

    Added directory iteration to find posts

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
 //! This code looks through all folders within the repo and converts them into
 //! a website
 
-use std::{error::Error, fs};
+mod file_system;
 
-const OUTPUT_PATH: &str = "./site";
+use std::{env, error::Error, fs, io::Write, path::PathBuf};
+
+use file_system::get_posts;
+
+#[derive(Default)]
+pub struct Config {
+    site_name: String,
+    input_dir: PathBuf,
+    output_dir: PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut config = Config::default();
+    config.input_dir = env::current_dir()?.canonicalize()?;
+    config.output_dir = PathBuf::from("./site").canonicalize()?;
+    config.site_name = config
+        .input_dir
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+
     // create a clean output directory
-    if fs::exists(OUTPUT_PATH)? {
-        fs::remove_dir_all(OUTPUT_PATH)?;
+    if fs::exists(&config.output_dir)? {
+        fs::remove_dir_all(&config.output_dir)?;
     }
-    fs::create_dir_all(OUTPUT_PATH)?;
+    fs::create_dir_all(&config.output_dir)?;
 
     // format individual posts
     // if the directory contains only an index file, create a single file, otherwise create a directory
 
     // create index pages
+    let mut index = fs::File::create(config.output_dir.join("index.html"))?;
+    let data = indoc::formatdoc! {"
+    <!doctype html>
+    <html>
+        <head>
+            <title>{name}</title>
+        </head>
+        <body>
+            {data:#?}
+        </body>
+    </html>
+    ",
+    name = config.site_name,
+    data = get_posts(&config)?};
+    write!(index, "{}", data)?;
 
     Ok(())
 }
